ffc6690dfd3f18d3553c782b55536eca80e59cde
minnnmin <38430523+minnnmin@users.noreply.github.com>
2022-12-25 15:01:23 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,8 +8,6 @@ data "aws_availability_zones" "available" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.10.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
 
   tags = {
     Name = "vpc"
